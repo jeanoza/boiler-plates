32130496ec588a3d1c40aa7292ef5a0b7e97b4b0
jeanoza <choikb916@gmail.com>
2023-12-29 21:41:49 +0100

    feat:PostView using fake data

---
+++ front-vue/src/components/TableComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th v-for="header in headers" :key="header">{{ header }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(post, index) in data" :key="index">
+        <td v-for="header in headers" :key="header" class="p-2">
+          {{ post[header] }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<script setup lang="ts" generic="T extends Record<string, unknown>">
+const { headers, data } = defineProps<{
+  headers: string[]
+  data: T[]
+}>()
+</script>
+<style scoped>
+/* TODO: styling:)) */
+.table {
+  @apply w-full table-fixed;
+}
+</style>
---
+++ front-vue/src/views/PostView.vue
@@ -0,0 +1,41 @@
+<template>
+  <MainLayout class="post">
+    <div class="w-1/2 max-w-4xl m-auto">
+      <div class="flex justify-end py-4">
+        <ButtonComponent label="New" />
+      </div>
+      <TableComponent v-if="FAKE_DATA" :data="FAKE_DATA" :headers="Object.keys(FAKE_DATA[0])" />
+    </div>
+  </MainLayout>
+</template>
+
+<script setup lang="ts">
+import ButtonComponent from '@/components/ButtonComponent.vue'
+import TableComponent from '@/components/TableComponent.vue'
+import { defineAsyncComponent } from 'vue'
+
+interface Post extends Record<string, unknown> {
+  name: string
+  content: string
+  created_by: string
+  created_at: string
+  updated_at: string
+}
+const FAKE_DATA: Post[] = [
+  {
+    name: 'Etre et Néant',
+    content: "L'existence précède l'essence.",
+    created_by: 'Jean-Paul Sartre',
+    created_at: '2021-01-01',
+    updated_at: '2021-01-01'
+  },
+  {
+    name: 'Le Deuxième Sexe',
+    content: 'Femme ne nait pas, elle le devient.',
+    created_by: 'Simon de Beauvoir',
+    created_at: '2021-01-01',
+    updated_at: '2021-01-01'
+  }
+]
+const MainLayout = defineAsyncComponent(() => import('@/components/layouts/MainLayout.vue'))
+</script>
